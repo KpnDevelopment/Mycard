60b168289f550418f4f076ef64c6a100d4121d91
kpndevelopment <vishnukpn884@gmail.com>
2020-05-02 21:19:41 +0530

    V1.0

--- app/src/main/java/com/example/mycard/Inserting_page.java
+++ app/src/main/java/com/example/mycard/Inserting_page.java
@@ -1,6 +1,7 @@
 package com.example.mycard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -16,6 +17,7 @@ public class Inserting_page extends AppCompatActivity {
     EditText state_txt;
     EditText location_txt;
     Button submit_btn;
+    Machine_ViewModel machine_viewModel;
 
 
     @Override
@@ -27,6 +29,7 @@ public class Inserting_page extends AppCompatActivity {
         state_txt=findViewById(R.id.state_txt);
         location_txt=findViewById(R.id.location_txt);
         submit_btn=findViewById(R.id.submit_btn);
+        machine_viewModel= new ViewModelProvider(this).get(Machine_ViewModel.class);
         submit_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,37 +44,6 @@ public class Inserting_page extends AppCompatActivity {
         final String state=state_txt.getText().toString().trim();
         final String location=location_txt.getText().toString().trim();
 
-
-    class Savetask extends AsyncTask<Void,Void,Void>{
-
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            //creating tasks
-            Machine_db machine_db=new Machine_db();
-
-            machine_db.setSerialno(serialno);
-            machine_db.setDistrict(district);
-            machine_db.setState(state);
-            machine_db.setLocation(location);
-            //adding to database
-            DatabaseClient.getInstance(getApplicationContext()).getAppdatbase().mechine_dao().insert(machine_db);
-
-            return null;
-
-
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-//            finish();
-            startActivity(new Intent(getApplicationContext(),Inserting_page.class));
-            Toast.makeText(getApplicationContext(),"saved",Toast.LENGTH_LONG).show();
-        }
-    }
-    Savetask savetask=new Savetask();
-    savetask.execute();
     }
 
 }
